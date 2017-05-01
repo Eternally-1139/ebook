61ba0a23e2322a0374e5053a1117a36e44708323
yifan <1139717977@qq.com>
2017-05-01 16:35:01 +0800

    asd

--- controllers/angular.go
+++ controllers/angular.go
@@ -17,6 +17,7 @@ func (this *AngularController) VueRender(){
 	if userinfo:=this.GetSession("userinfo");userinfo!=nil{
 
 		this.Ctx.Redirect(302,"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfcb057b3c57cee69&redirect_uri=http://ebook.hnhqjk.com/api/user/loginAuto&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect")
+		return 
 
 	}else{
 		this.TplName="angular/index.html"
