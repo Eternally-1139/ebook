9dac6b211a4756f56aec3fc77880d2cdca85f6e6
yifan <1139717977@qq.com>
2017-05-01 16:50:15 +0800

    Asda

--- controllers/angular.go
+++ controllers/angular.go
@@ -27,7 +27,7 @@ func (this *AngularController) AngularRender(){
 	}
 
 	if !this.IsLogin{
-		this.Ctx.Redirect(302,"/admin")
+		this.Ctx.Redirect(302,"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfcb057b3c57cee69&redirect_uri=http://ebook.hnhqjk.com/api/user/loginAuto&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect")
 		return
 	}else{
 		user:=this.GetSession("users").(models.User)
