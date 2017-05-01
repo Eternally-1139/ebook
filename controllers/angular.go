ebda2ba27111a31f6bd608510cc8026ed4052a83
yifan <1139717977@qq.com>
2017-05-01 16:49:10 +0800

    dasda

--- controllers/angular.go
+++ controllers/angular.go
@@ -9,6 +9,8 @@ import (
 
 type AngularController struct{
 	beego.Controller
+	IsLogin bool
+	User models.User
 }
 
 // @router /* [*]
@@ -21,11 +23,17 @@ func (this *AngularController) AngularRender(){
 			this.Ctx.Redirect(302,"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfcb057b3c57cee69&redirect_uri=http://ebook.hnhqjk.com/api/user/loginAuto&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect")
 			return
 		}
-		return
 
+	}
+
+	if !this.IsLogin{
+		this.Ctx.Redirect(302,"/admin")
+		return
 	}else{
 		user:=this.GetSession("users").(models.User)
 		fmt.Println("已登录用户："+user.Name)
 		this.TplName="angular/index.html"
 	}
+
+
 }
