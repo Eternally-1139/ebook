6258dd69297f257141c4f4228d10e84b09077e93
yifan <1139717977@qq.com>
2017-05-01 22:45:05 +0800

    Asdasd

--- controllers/angular.go
+++ controllers/angular.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"ebook/models"
-	"fmt"
 )
 
 
@@ -18,14 +17,9 @@ func (this *AngularController) AngularRender(){
 
 
 	if userinfo:=this.GetSession("users");userinfo!=nil{
-		user:=userinfo.(models.User)
-		if err:=user.Read();err!=nil{
-			this.SetSession("users",user)
-			this.IsLogin = true
-			this.TplName="angular/index.html"
-		}else{
-			fmt.Println("用户读取失败")
-		}
+		this.TplName="angular/index.html"
+		return
+
 
 	}else{
 		this.Ctx.Redirect(302,"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfcb057b3c57cee69&redirect_uri=http://ebook.hnhqjk.com/api/user/loginAuto&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect")
