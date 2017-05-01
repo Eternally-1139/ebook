e58a5c3909b98915a02ddb419882551134fd03d2
yifan <1139717977@qq.com>
2017-05-01 16:36:45 +0800

    Sdfsf

--- controllers/angular.go
+++ controllers/angular.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 
+	"fmt"
 )
 
 
@@ -11,15 +12,16 @@ type AngularController struct{
 }
 
 // @router /* [*]
-func (this *AngularController) VueRender(){
+func (this *AngularController) AngularRender(){
 
 
 	if userinfo:=this.GetSession("userinfo");userinfo!=nil{
 
 		this.Ctx.Redirect(302,"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfcb057b3c57cee69&redirect_uri=http://ebook.hnhqjk.com/api/user/loginAuto&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect")
-		return 
+		return
 
 	}else{
+		fmt.Println("已登录用户："+this.GetSession("userinfo"))
 		this.TplName="angular/index.html"
 	}
 }
